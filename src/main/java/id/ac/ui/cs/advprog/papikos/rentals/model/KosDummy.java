9418617daf08d4e802a3f83ed01a76f470d5e26b
albyarto <albyarto.07@gmail.com>
2025-04-11 19:42:59 +0700

    [SETUP] Add dummy models for Kos and Tenant

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/rentals/model/KosDummy.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.papikos.rentals.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// Model dummy untuk Objek Kos (Sementara)
+public class KosDummy {
+    private String id;
+    private String name;
+    private String address;
+    private String description;
+    private int totalRooms;
+    private int availableRooms;
+    private String pricePerMonth;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/rentals/model/TenantDummy.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.papikos.rentals.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// Model dummy untuk Objek Tenant (Sementara)
+public class TenantDummy {
+    private String id;
+    private String email;
+    private String role;
+    private String status;
+}
