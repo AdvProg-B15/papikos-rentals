6a635af6bb2ff714cac704aab5a98eaea22722e0
albyarto <albyarto.07@gmail.com>
2025-05-09 10:47:50 +0700

    feat: add custom exception

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/rentals/exception/ForbiddenException.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.papikos.rentals.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
